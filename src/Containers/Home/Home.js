a48d9252c85944e22c0e31d68423249898e5fd6b
wenitteapiou <wenitteapiou@gmail.com>
2022-08-15 10:31:53 -0400

    styling + added PDF download link for Alphabet

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -6,10 +6,11 @@ import {Link} from 'react-router-dom';
 
 function Home() {
   
-
     return (
         <div className="App">
       <div className="App-body">
+      <p style={{lineHeight:"40px", textAlign:"left", margin:"50px"}}>
+
       The Gur languages, also known as Central Gur or Mabia, belong to the Niger–Congo language family. There are about 70 languages belonging to this group. They are spoken in the Sahelian and savanna regions of West Africa, namely: in most areas of Burkina Faso, and in south-central Mali, northeastern Ivory Coast, the northern halves of Ghana and Togo, northwestern Benin, and southwestern Niger. Additionally, a single Gur language, Baatonum, is spoken in the extreme northwest of Nigeria. Like most Niger–Congo languages, the ancestor of Gur languages probably had a noun class system; many of today's languages have reduced this to a system of nominal genders or declensions or no longer have a class system.A common property of Gur languages is the verbal aspect marking. Almost all Gur languages are tonal, with Koromfé being a notable exception. The tonal systems of Gur languages are rather divergent. Most Gur languages have been described as following the model of a two tone downstep system, but the languages of Oti-Volta branch and some others have three phonemic tones.
 
 The most spoken Gur language is Mooré, the language of the Mossi people. Mooré is spoken by over 12 million people in Burkina Faso, plus another 5M+ in surrounding countries such as Ghana, Cote D'ivoire, Niger, Mali and Togo as a native language, but with many more L2 speakers. Mooré is spoken as a first or second language by over 50% of the Burkinabè population .
@@ -19,8 +20,10 @@ The colonization of Africa and conquering of Gur speaking nations have led to th
 Goulsse (meaning writing in Mooré) is a modern script for writing Gur languages. The script was developed by Wenitte Apiou and Babaguioue Micareme Akouabou in 2022. Born in Burkina Faso to a Kassena father and a Mossi mother, Apiou was a student of Computer Science at Stanford University when he contacted Babaguioue, also Burkinabè Kassena, and a doctoral candidate in Linguistics with research focus on the Kassena language at the University of Ouagadougou.  The script is an alphabet with 30 letters. Goulsse draws inspiration from ancient writing systems used in the Volta region where Gur languages are spoken, and represents the community’s idea of how these writing systems may have further evolved if Gur peoples had never been colonized. Goulsse is written from left to right, has no uppercase or lowercase, and uses the standard international Hindu-Arabic numerals. 
 
 To start writing in Goulsse today download the font at https://goulsse.org. It can also be typed using the Mandla keyboard input tool at https://keyboards.mandla.ai/About/Goulsse/
+</p>
 </div>
 <Link to="/files/goulsse.ttf" target="_blank" download>Download Goulsse font today</Link>
+<Link to="/files/Gʋlse.pdf" target="_blank" download>Download the Goulsse Alphabet PDF</Link>
     </div>
     );
   }
