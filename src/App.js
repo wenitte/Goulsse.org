20ce2c6d43067cd24a2322b2cd0cd29263e9f8ed
wenitteapiou <wenitteapiou@gmail.com>
2022-08-15 08:33:41 -0400

    added favicon + changed logo

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route,   BrowserRouter} from 'react-router-dom';
+import Home from './Containers/Home/Home.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        The Gur languages, also known as Central Gur or Mabia, belong to the Niger–Congo language family. There are about 70 languages belonging to this group. They are spoken in the Sahelian and savanna regions of West Africa, namely: in most areas of Burkina Faso, and in south-central Mali, northeastern Ivory Coast, the northern halves of Ghana and Togo, northwestern Benin, and southwestern Niger. Additionally, a single Gur language, Baatonum, is spoken in the extreme northwest of Nigeria. Like most Niger–Congo languages, the ancestor of Gur languages probably had a noun class system; many of today's languages have reduced this to a system of nominal genders or declensions or no longer have a class system.A common property of Gur languages is the verbal aspect marking. Almost all Gur languages are tonal, with Koromfé being a notable exception. The tonal systems of Gur languages are rather divergent. Most Gur languages have been described as following the model of a two tone downstep system, but the languages of Oti-Volta branch and some others have three phonemic tones.
+<>
+<BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-The most spoken Gur language is Mooré, the language of the Mossi people. Mooré is spoken by over 12 million people in Burkina Faso, plus another 5M+ in surrounding countries such as Ghana, Cote D'ivoire, Niger, Mali and Togo as a native language, but with many more L2 speakers. Mooré is spoken as a first or second language by over 50% of the Burkinabè population .
-
-The colonization of Africa and conquering of Gur speaking nations have led to the marginalization  of indegenous writing systems and the endangerment of Gur languages. Colonial governments  prioritized the use of European colonial languages such as English and French which use the latin script, and today most Gur languages are written with modified latin scripts, Ajami scripts, or N’ko script. None of these writing systems are well suited to the tones unique to Gur languages. The natural evolution of pre colonial Gur writing systems was unfortunately stifled by colonial regimes and lack of government desire to promote indegenous writing systems post independence.
-
-Goulsse (meaning writing in Mooré) is a modern script for writing Gur languages. The script was developed by Wenitte Apiou and Babaguioue Micareme Akouabou in 2022. Born in Burkina Faso to a Kassena father and a Mossi mother, Apiou was a student of Computer Science at Stanford University when he contacted Babaguioue, also Burkinabè Kassena, and a doctoral candidate in Linguistics with research focus on the Kassena language at the University of Ouagadougou.  The script is an alphabet with 30 letters. Goulsse draws inspiration from ancient writing systems used in the Volta region where Gur languages are spoken, and represents the community’s idea of how these writing systems may have further evolved if Gur peoples had never been colonized. Goulsse is written from left to right, has no uppercase or lowercase, and uses the standard international Hindu-Arabic numerals. 
-
-To start writing in Goulsse today download the font at https://goulsse.org. It can also be typed using the Mandla keyboard input tool at https://keyboards.mandla.ai/About/Goulsse/
-
- 
-
-
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        </Routes>
+      </div>
+    </BrowserRouter>
+</>
   );
 }
 
